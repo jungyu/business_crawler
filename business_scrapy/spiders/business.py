a329f214d6a6a5deb9c2ea6ca102805a66b5cf23
jungyu <jungyuyu@gmail.com>
2020-07-30 14:02:55 +0800

    Test demo site

--- business_scrapy/spiders/business.py
+++ business_scrapy/spiders/business.py
@@ -3,8 +3,18 @@ import scrapy
 
 class BusinessSpider(scrapy.Spider):
     name = 'business'
-    allowed_domains = ['example.com']
-    start_urls = ['http://example.com/']
+
+    def start_requests(self):
+        urls = [
+            'http://quotes.toscrape.com/page/1/',
+            'http://quotes.toscrape.com/page/2/',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        pass
+        page = response.url.split("/")[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
