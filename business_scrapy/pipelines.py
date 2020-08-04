44c97dd08ce56390cfb6de8b9fe9924a7bd651be
jungyu <jungyuyu@gmail.com>
2020-08-04 16:35:10 +0800

    '完成crawler_source的讀取'

--- business_scrapy/pipelines.py
+++ business_scrapy/pipelines.py
@@ -5,14 +5,54 @@
 
 
 # useful for handling different item types with a single interface
+import loguru
+import json
+
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 from itemadapter import ItemAdapter
-from database.connection import session
-from database.models import ListData, ArticleData
+
+import sqlalchemy
+import sqlalchemy.ext.automap
+from ..database.connection import session, metadata, automap
 
 
 class BusinessScrapyPipeline:
+    __articletable__ = 'crawler_article'
+    __articlemetatable__ = 'crawler_articlemeta'
+    __fieldstable__ = 'crawler_fields'
+    __listtable__ = 'crawler_list'
+    __mediatable__ = 'crawler_media'
+    
     def process_item(self, item, spider):
+        global source
+        created = int(time.mktime(datetime.now().timetuple()))
 
+        sqlalchemy.Table(self.__listtable__, metadata, autoload=True)
+        sqlalchemy.Table(self.__articletable__, metadata, autoload=True)
+
+        Article = automap.classes[self.__articletable__]
+        Alist = automap.classes[self.__listtable__]
+        loguru.logger.info(source)
+        '''
+        Q1.如何跨類傳遞參數
+        Q2.如何還沒 commit 前，取得 list id
+        alist = Alist()
+        alist.source_id = source.id
+        alist.topic = source.topics #TOFIX
+        alist.article_title = title
+        alist.article_url = perma_link
+        alist.created = created
+        session.add(alist)
+
+        article = Article()
+        article.title = item['title'].encode('utf-8')
+        article.source_title = item['source_content'].encode('utf-8')
+        session.add(article)
+        '''
+
+        
         return item
 
     def close_spider(self, spider):
--- business_scrapy/spiders/business.py
+++ business_scrapy/spiders/business.py
@@ -1,4 +1,5 @@
 import loguru
+import json
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
@@ -7,6 +8,7 @@ import sqlalchemy.ext.automap
 
 from ..database.connection import session, metadata, automap
 
+source = None
 
 class BusinessSpider(scrapy.Spider):
     name = 'business'
@@ -14,28 +16,34 @@ class BusinessSpider(scrapy.Spider):
     user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36'
  
     def get_source_path(self, sourceId):
+        global source
         sqlalchemy.Table(self.__sourcetable__, metadata, autoload=True)
         Source = automap.classes[self.__sourcetable__]
-        self.source = session.query(Source).filter(
+        source = session.query(Source).filter(
             Source.id == int(sourceId)
         ).one()
-        loguru.logger.info(f'{self.source.topics}-{self.source.name}')
-        loguru.logger.info(f'{self.source.crawler_schema}')
+        self.schema = json.loads(source.crawler_schema)
+        #loguru.logger.info(f'{source.topics}-{source.name}')
+        #loguru.logger.info(f'{schema["page_title"]}')
         self.rules = {
-            Rule(LinkExtractor(restrict_xpaths="//a[@class='browse-recipe-link']/@href"), callback='parse_item', follow=True, process_request='set_user_agent'),
-            Rule(LinkExtractor(restrict_xpaths="//a[@class='pagination-tab-link']/@href"), process_request='set_user_agent')
+            Rule(LinkExtractor(restrict_xpaths=self.schema["page_title"]), callback='parse_item', follow=True, process_request='set_user_agent'),
+            Rule(LinkExtractor(restrict_xpaths=self.schema["next_page"]), process_request='set_user_agent')
         }
 
     def start_requests(self):
+        global source
         sourceId = getattr(self, 'id', None)
         if sourceId is not None:
             self.get_source_path(sourceId)
-            yield scrapy.Request(url=self.source.crawler_url, callback=self.parse)
+            yield scrapy.Request(url=source.crawler_url, callback=self.parse)
         else:
             print('Please add parameter at command, e.g. scrapy crawl business -a id=5')
 
     def parse(self, response):
-        print('parse()')
+        yield{
+            'title':response.xpath(self.schema["title"]).get(),
+            'source_content':(response.xpath(self.schema["source_content"]).get()).replace("\n","")
+        }
 
 
         
