1124997f5fa7a9a723e74728f8729d60d8cf217f
jungyu <jungyuyu@gmail.com>
2020-08-05 17:03:14 +0800

    可以存資料庫了，但只存了一次

--- business_scrapy/pipelines.py
+++ business_scrapy/pipelines.py
@@ -7,6 +7,8 @@
 # useful for handling different item types with a single interface
 import loguru
 import json
+import time
+from datetime import datetime
 
 import scrapy
 from scrapy.linkextractors import LinkExtractor
@@ -15,7 +17,7 @@ from itemadapter import ItemAdapter
 
 import sqlalchemy
 import sqlalchemy.ext.automap
-from ..database.connection import session, metadata, automap
+from .database.connection import session, metadata, automap
 
 class BusinessScrapyPipeline:
     __articletable__ = 'crawler_article'
@@ -25,6 +27,7 @@ class BusinessScrapyPipeline:
     __mediatable__ = 'crawler_media'
     
     def process_item(self, item, spider):
+        loguru.logger.info('process_item()')
         created = int(time.mktime(datetime.now().timetuple()))
 
         sqlalchemy.Table(self.__listtable__, metadata, autoload=True)
@@ -32,25 +35,24 @@ class BusinessScrapyPipeline:
 
         Article = automap.classes[self.__articletable__]
         Alist = automap.classes[self.__listtable__]
-        loguru.logger.info(source)
 
         alist = Alist()
         alist.source_id = item['source_id']
-        alist.topic = item['source_id']
-        alist.article_title = item['title'].encode('utf-8')
+        alist.topics = item['source_id']
+        alist.article_title = item['title']
         alist.article_url = item['article_url']
         alist.created = created
         session.add(alist)
         session.flush()
-        '''
+
         article = Article()
         article.list_id = alist.id
         article.source_url = item['article_url']
-        article.title = item['title'].encode('utf-8')
-        article.source_content = item['source_content'].encode('utf-8')
+        article.title = item['title']
+        article.source_content = item['source_content']
         article.created = created
         session.add(article)
-        '''
+
         return item
 
     def close_spider(self, spider):
