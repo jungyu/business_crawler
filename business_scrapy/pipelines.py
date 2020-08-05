f9d481b138d8f3760c8589b5f01740625452d2c2
jungyu <jungyuyu@gmail.com>
2020-08-05 10:52:26 +0800

    pass variable to pipeline

--- business_scrapy/pipelines.py
+++ business_scrapy/pipelines.py
@@ -17,7 +17,6 @@ import sqlalchemy
 import sqlalchemy.ext.automap
 from ..database.connection import session, metadata, automap
 
-
 class BusinessScrapyPipeline:
     __articletable__ = 'crawler_article'
     __articlemetatable__ = 'crawler_articlemeta'
@@ -26,7 +25,6 @@ class BusinessScrapyPipeline:
     __mediatable__ = 'crawler_media'
     
     def process_item(self, item, spider):
-        global source
         created = int(time.mktime(datetime.now().timetuple()))
 
         sqlalchemy.Table(self.__listtable__, metadata, autoload=True)
@@ -35,30 +33,32 @@ class BusinessScrapyPipeline:
         Article = automap.classes[self.__articletable__]
         Alist = automap.classes[self.__listtable__]
         loguru.logger.info(source)
-        '''
-        Q1.如何跨類傳遞參數
-        Q2.如何還沒 commit 前，取得 list id
+
         alist = Alist()
-        alist.source_id = source.id
-        alist.topic = source.topics #TOFIX
-        alist.article_title = title
-        alist.article_url = perma_link
+        alist.source_id = item['source_id']
+        alist.topic = item['source_id']
+        alist.article_title = item['title'].encode('utf-8')
+        alist.article_url = item['article_url']
         alist.created = created
         session.add(alist)
-
+        session.flush()
+        '''
         article = Article()
+        article.list_id = alist.id
+        article.source_url = item['article_url']
         article.title = item['title'].encode('utf-8')
-        article.source_title = item['source_content'].encode('utf-8')
+        article.source_content = item['source_content'].encode('utf-8')
+        article.created = created
         session.add(article)
         '''
-
-        
         return item
 
     def close_spider(self, spider):
         try:
             session.commit()
-        except:
+        except Exception as e:
+            loguru.logger.error('新增資料失敗')
+            loguru.logger.error(e)
             session.rollback()
         finally:
             session.close()
--- business_scrapy/spiders/business.py
+++ business_scrapy/spiders/business.py
@@ -8,22 +8,19 @@ import sqlalchemy.ext.automap
 
 from ..database.connection import session, metadata, automap
 
-source = None
-
 class BusinessSpider(scrapy.Spider):
     name = 'business'
     __sourcetable__ = 'crawler_source'
     user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36'
  
     def get_source_path(self, sourceId):
-        global source
         sqlalchemy.Table(self.__sourcetable__, metadata, autoload=True)
         Source = automap.classes[self.__sourcetable__]
-        source = session.query(Source).filter(
+        self.source = session.query(Source).filter(
             Source.id == int(sourceId)
         ).one()
-        self.schema = json.loads(source.crawler_schema)
-        #loguru.logger.info(f'{source.topics}-{source.name}')
+        self.schema = json.loads(self.source.crawler_schema)
+        #loguru.logger.info(f'{self.source.topics}-{self.source.name}')
         #loguru.logger.info(f'{schema["page_title"]}')
         self.rules = {
             Rule(LinkExtractor(restrict_xpaths=self.schema["page_title"]), callback='parse_item', follow=True, process_request='set_user_agent'),
@@ -31,16 +28,18 @@ class BusinessSpider(scrapy.Spider):
         }
 
     def start_requests(self):
-        global source
         sourceId = getattr(self, 'id', None)
         if sourceId is not None:
             self.get_source_path(sourceId)
-            yield scrapy.Request(url=source.crawler_url, callback=self.parse)
+            yield scrapy.Request(url=self.source.crawler_url, meta={'source_id':self.source.id, 'topics':self.source.topics}, callback=self.parse)
         else:
             print('Please add parameter at command, e.g. scrapy crawl business -a id=5')
 
     def parse(self, response):
         yield{
+            'source_id':response.meta['source_id'],
+            'topics':response.meta['topics'],
+            'article_url':response.url,
             'title':response.xpath(self.schema["title"]).get(),
             'source_content':(response.xpath(self.schema["source_content"]).get()).replace("\n","")
         }
